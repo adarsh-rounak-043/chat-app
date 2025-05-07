4f7eef6aabeb54657623bcad2cbd31367c78ad0d
adarsh-rounak-043 <adarsh.rounak.043@gmail.com>
2025-05-08 02:44:32 +0530

    Changes in config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Ensure Vite binds to the correct network
+    port: process.env.PORT || 3000, // Use Render's PORT variable
+  }
 })
